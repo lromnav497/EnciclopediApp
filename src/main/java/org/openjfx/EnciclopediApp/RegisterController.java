17cfddbedd55d891ab13705d82d05008638a8a79
lromnav497 <146075035+lromnav497@users.noreply.github.com>
2024-04-29 11:28:32 +0200

    app principal- fmxl

---
+++ src/main/java/org/openjfx/EnciclopediApp/RegisterController.java
@@ -0,0 +1,46 @@
+package org.openjfx.EnciclopediApp;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class RegisterController {
+
+	@FXML
+	private TextField nombre;
+	@FXML
+	private TextField apellido;
+	@FXML
+	private DatePicker fechaNacimiento;
+	@FXML
+	private TextField correo;
+	@FXML
+	private TextField telefono;
+	@FXML
+	private PasswordField contrasena;
+	@FXML
+	private PasswordField confirmarContrasena;
+	@FXML
+	private CheckBox afiliado;
+	@FXML
+	private CheckBox aceptarPublicidad;
+	@FXML
+	private Label errorLabel;
+
+	@FXML
+	protected void handleRegisterButtonAction(ActionEvent event) {
+		String password = contrasena.getText();
+		String confirmPassword = confirmarContrasena.getText();
+
+		if (!password.equals(confirmPassword)) {
+			errorLabel.setText("Las contraseñas no coinciden");
+			errorLabel.setVisible(true);
+		} else {
+			// Procesar el registro
+		}
+	}
+}
